7ceb545e98998df69f8fd47ffda3729569f48014
SAMURAI JACK <mr.amir1385mm@gmail.com>
2023-09-19 19:39:58 +0330

    updated api files

--- backend/dbconfig/api.php
+++ backend/dbconfig/api.php
@@ -2,10 +2,11 @@
 
 header("Content-Type:application/json");
 
-if (isset($_GET)) {
+if (isset($_GET) && $_GET['id'] == "user") {
+
 
     include('database.php');
-    
+
     $result = $con->prepare("SELECT * FROM `user` ");
     $result->execute();
     $datas = $result->fetchAll(PDO::FETCH_ASSOC);
@@ -22,21 +23,55 @@ if (isset($_GET)) {
         $birth_date = $data['birth date'];
         response($id, $name, $password, $email, $phone_number, $home_number, $national_code, $card_number, $birth_date);
     }
-} else {
-    response(NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 200, "No Record Found");
-}
+    function response($id, $name, $password, $email, $phone_number, $home_number, $national_code, $card_number, $birth_date)
+    {
+        $response['id'] = $id;
+        $response['name'] = $name;
+        $response['password'] = $password;
+        $response['email'] = $email;
+        $response['phone_number'] = $phone_number;
+        $response['home_number'] = $home_number;
+        $response['national_code'] = $national_code;
+        $response['card_number'] = $card_number;
+        $response['birth_date'] = $birth_date;
+        $json_response = json_encode($response);
+        echo $json_response;
+    }
+} elseif (isset($_GET) && $_GET['id'] == "product") {
 
-function response($id, $name, $password, $email, $phone_number, $home_number, $national_code, $card_number, $birth_date)
-{
-    $response['id'] = $id;
-    $response['name'] = $name;
-    $response['password'] = $password;
-    $response['email'] = $email;
-    $response['phone_number'] = $phone_number;
-    $response['home_number'] = $home_number;
-    $response['national_code'] = $national_code;
-    $response['card_number'] = $card_number;
-    $response['birth_date'] = $birth_date;
-    $json_response = json_encode($response);
-    echo $json_response;
+    include('database.php');
+
+    $result = $con->prepare("SELECT * FROM `product` ");
+    $result->execute();
+    $datas = $result->fetchAll(PDO::FETCH_ASSOC);
+
+
+    foreach ($datas as $data) {
+        $title = $data['title'];
+        $image = $data['image'];
+        $content = $data['content'];
+        $price = $data['price'];
+        $warranty = $data['warranty'];
+        $score = $data['score'];
+        $tags = $data['tags'];
+        $meta = $data['meta'];
+        response($id, $title, $image, $content, $price, $warranty, $score, $tags, $meta);
+    }
+
+    function response($id, $title, $image, $content, $price, $warranty, $score, $tags, $meta)
+    {
+        $response['id'] = $id;
+        $response['title'] = $title;
+        $response['image'] = $image;
+        $response['content'] = $content;
+        $response['price'] = $price;
+        $response['warranty'] = $warranty;
+        $response['score'] = $score;
+        $response['tags'] = $tags;
+        $response['meta'] = $meta;
+        $json_response = json_encode($response);
+        echo $json_response;
+    }
+} else {
+    response(NULL, NULL, NULL, NULL, NULL, NULL, NULL, 200, "No Record Found");
 }
