b13d322f105abaa9532e7c13741c824006193e81
Ali <ali.tajik.1385.a@gmail.com>
2023-10-30 22:52:32 +0330

    basket page is responsive

--- frontend/src/component/Basket/Basket.jsx
+++ frontend/src/component/Basket/Basket.jsx
@@ -17,34 +17,34 @@ export default function Basket() {
     return (
         <div>
             <HeaderPage title="سبد خرید" subtitle="مدیریت و خرید همزمان چند محصول" route="خانه" />
-            <div className=" flex w-[90%] mt-4 mx-auto p-3 items-start   gap-x-3">
-                <div className="w-1/4 flex flex-col items-center  border border-gray-50 rounded-sm p-2 space-y-4 ">
-                    <div className='flex w-full m-2 flex-row-reverse justify-between text-lg rounded-sm font-bold p-3'>
+            <div className=" flex w-[95%] flex-col-reverse lg:flex-row mt-4 mx-auto p-3 items-start   gap-x-3">
+                <div className="w-full lg:w-1/4 flex flex-col items-center  border border-gray-50 rounded-sm p-2 space-y-4 ">
+                    <div className='flex w-full m-2 flex-row-reverse justify-between  lg:text-sm  rounded-sm font-bold p-3'>
                         <span> : جمع کل فاکتور</span>
                         <span>3.200.000 تومان</span>
                     </div>
-                    <div className='flex w-full m-2 flex-row-reverse justify-between text-lg  bg-gray-100  font-bold p-3'>
+                    <div className='flex w-full m-2 flex-row-reverse justify-between  lg:text-sm   bg-gray-100  font-bold p-3'>
                         <span> : جمع تخفیف</span>
                         <span>200.000 تومان</span>
                     </div>
-                    <div className='flex w-full m-2 flex-row-reverse justify-between text-lg rounded-sm text-green-600 font-bold p-3'>
+                    <div className='flex w-full m-2 flex-row-reverse justify-between  lg:text-sm  rounded-sm text-green-600 font-bold p-3'>
                         <span> : مبلغ قابل پرداخت </span>
                         <span>3.000.000 تومان</span>
                     </div>
-                    <button className='w-[80%] p-2 mx-auto my-2 bg-green-700 text-white font-bold text-xl rounded-lg hover:bg-green-800 transition-colors duration-300' > <Link to="/factor" >  ادامه ثبت سفارش </Link></button>
+                    <button className='w-[80%] p-2 mx-auto my-2 bg-green-700 text-white font-bold  lg:text-sm  rounded-lg hover:bg-green-800 transition-colors duration-300' > <Link to="/factor" >  ادامه ثبت سفارش </Link></button>
                 </div>
-                <div className="w-3/4 border border-gray-50 rounded-sm p-3 space-y-4">
+                <div className="w-full lg:w-3/4 border border-gray-50 rounded-sm p-3 space-y-4">
                     <p className='w-full text-right p2 text-lg font-semibold '>
                         می‌خواهید محصولات دیگری اضافه کنید؟
                         <Link to="/" className='text-mainPrimary mr-2' >بازگشت به فروشگاه</Link>
                     </p>
-                    <table className=' flex flex-col justify-center items-end text-lg  font-semibold w-full space-y-2 '>
+                    <table className=' flex flex-col justify-center items-end text-sm xl:text-lg  font-semibold w-full space-y-2 '>
                         <thead className='w-full'>
                             <th className=' flex flex-row-reverse items-center justify-between px-2   rounded-sm bg-slate-200 font-semibold'>
                                 <td className='w-1/5 text-end mr-2'>کالا</td>
                                 <td>قیمت واحد</td>
                                 <td>تعداد</td>
-                                <td>تخفیف</td>
+                                <td className='md:block hidden'>تخفیف</td>
                                 <td>قیمت نهایی</td>
                             </th>
                         </thead>
@@ -54,37 +54,15 @@ export default function Basket() {
                                     <SideProductBox title="گوشی موبایل سامسونگ مدل Galaxy A21s
                                             دارای قابلیت دو سیم کارته و حافظه 128 گیگا بایت" img="phone1.webp" price="" />
                                 </td>
-                                <td>
+                                <td >
                                     3.200.000 تومان
                                 </td>
-                                <td className='flex justify-center items-center space-x-2'>
-                                    <span onClick={PlusCount} className='text-xl flex items-center justify-center font-bold  w-10 h-12  bg-slate-100 rounded-sm cursor-pointer border border-slate-50'>+</span>
-                                    <input type="text" value={count} disabled className='w-12 h-12 bg-slate-100 rounded-sm  text-center' />
-                                    <span onClick={MinusCount} className='text-xl flex items-center justify-center font-bold  w-10 h-12  bg-slate-100 rounded-sm  cursor-pointer border border-slate-50'>-</span>
+                                <td className='flex flex-col md:flex-row justify-center items-center space-y-2 md:space-x-2'>
+                                    <span onClick={PlusCount} className='text-base lg:text-xl flex items-center justify-center font-bold    w-8 h-10  xl:w-10 xl:h-12  bg-slate-100 rounded-sm cursor-pointer border border-slate-50'>+</span>
+                                    <input type="text" value={count} disabled className=' w-10 h-10  xl:w-12 xl:h-12 bg-slate-100 rounded-sm  text-center' />
+                                    <span onClick={MinusCount} className='text-base lg:text-xl flex items-center justify-center font-bold   w-8 h-10  xl:w-10 xl:h-12  bg-slate-100 rounded-sm  cursor-pointer border border-slate-50'>-</span>
                                 </td>
-                                <td>
-                                    3.200.000 تومان
-
-                                </td>
-                                <td className='flex flex-col-reverse items-center justify-center'>
-                                    <button className='m-2 text-white bg-red-500 px-3 py-2 hover:bg-red-600  rounded-lg text-sm'>حذف </button>       3.000.000 تومان
-                                </td>
-
-                            </tr>
-                            <tr className='flex flex-row-reverse w-full h-auto items-center justify-between p-2 border-b border-b-gray-300 m-1'>
-                                <td className='w-[30%]'>
-                                    <SideProductBox title="گوشی موبایل سامسونگ مدل Galaxy A21s
-                                            دارای قابلیت دو سیم کارته و حافظه 128 گیگا بایت" img="phone1.webp" price="" />
-                                </td>
-                                <td>
-                                    3.200.000 تومان
-                                </td>
-                                <td className='flex justify-center items-center space-x-2'>
-                                    <span onClick={PlusCount} className='text-xl flex items-center justify-center font-bold  w-10 h-12  bg-slate-100 rounded-sm cursor-pointer border border-slate-50'>+</span>
-                                    <input type="text" value={count} disabled className='w-12 h-12 bg-slate-100 rounded-sm  text-center' />
-                                    <span onClick={MinusCount} className='text-xl flex items-center justify-center font-bold  w-10 h-12  bg-slate-100 rounded-sm  cursor-pointer border border-slate-50'>-</span>
-                                </td>
-                                <td>
+                                <td className='md:block hidden'>
                                     3.200.000 تومان
 
                                 </td>
@@ -93,50 +71,7 @@ export default function Basket() {
                                 </td>
 
                             </tr>
-                            <tr className='flex flex-row-reverse w-full h-auto items-center justify-between p-2 border-b border-b-gray-300 m-1'>
-                                <td className='w-[30%]'>
-                                    <SideProductBox title="گوشی موبایل سامسونگ مدل Galaxy A21s
-                                          دارای قابلیت دو سیم کارته و حافظه 128 گیگا بایت" img="phone1.webp" price="" />
-                                </td>
-                                <td>
-                                    3.200.000 تومان
-                                </td>
-                                <td className='flex justify-center items-center space-x-2'>
-                                    <span onClick={PlusCount} className='text-xl flex items-center justify-center font-bold  w-10 h-12  bg-slate-100 rounded-sm cursor-pointer border border-slate-50'>+</span>
-                                    <input type="text" value={count} disabled className='w-12 h-12 bg-slate-100 rounded-sm  text-center' />
-                                    <span onClick={MinusCount} className='text-xl flex items-center justify-center font-bold  w-10 h-12  bg-slate-100 rounded-sm  cursor-pointer border border-slate-50'>-</span>
-                                </td>
-                                <td>
-                                    3.200.000 تومان
 
-                                </td>
-                                <td className='flex flex-col-reverse items-center justify-center'>
-                                    <button className='m-2 text-white bg-red-500 px-3 py-2 hover:bg-red-600  rounded-lg text-sm'>حذف </button>       3.000.000 تومان
-                                </td>
-
-                            </tr>
-                            <tr className='flex flex-row-reverse w-full h-auto items-center justify-between p-2 border-b border-b-gray-300 m-1'>
-                                <td className='w-[30%]'>
-                                    <SideProductBox title="گوشی موبایل سامسونگ مدل Galaxy A21s
-                                دارای قابلیت دو سیم کارته و حافظه 128 گیگا بایت" img="phone1.webp" price="" />
-                                </td>
-                                <td>
-                                    3.200.000 تومان
-                                </td>
-                                <td className='flex justify-center items-center space-x-2'>
-                                    <span onClick={PlusCount} className='text-xl flex items-center justify-center font-bold  w-10 h-12  bg-slate-100 rounded-sm cursor-pointer border border-slate-50'>+</span>
-                                    <input type="text" value={count} disabled className='w-12 h-12 bg-slate-100 rounded-sm  text-center' />
-                                    <span onClick={MinusCount} className='text-xl flex items-center justify-center font-bold  w-10 h-12  bg-slate-100 rounded-sm  cursor-pointer border border-slate-50'>-</span>
-                                </td>
-                                <td>
-                                    3.200.000 تومان
-
-                                </td>
-                                <td className='flex flex-col-reverse items-center justify-center'>
-                                    <button className='m-2 text-white bg-red-500 px-3 py-2 hover:bg-red-600  rounded-lg text-sm'>حذف </button>       3.000.000 تومان
-                                </td>
-
-                            </tr>
                         </tbody>
                     </table>
 
--- frontend/src/component/ProductBox/SideProductBox.jsx
+++ frontend/src/component/ProductBox/SideProductBox.jsx
@@ -5,10 +5,10 @@ export default function SideProductBox({ img, title, price }) {
     return (
         <div>
             <Link to="/products/3">
-                <div className="w-full h-28 flex flex-row-reverse items-start justify-between hover:opacity-90 overflow-hidden">
+                <div className="w-full h-28 flex flex-col md:flex-row-reverse items-center justify-between hover:opacity-90 overflow-hidden">
                     <img src={`../images/${img}`} alt="" className='w-24 h-3/4 rounded-sm ' />
                     <span className='flex flex-col items-end space-y-1 justify-end mr-2'>
-                        <h1 className='text-sm text-mainPrimaryLight font-bold text-right line-clamp-2'>{title}</h1>
+                        <h1 className='text-sm text-mainPrimaryLight font-bold text-right line-clamp-4'>{title}</h1>
                         <p className='text-base font-semibold text-mainPrimary text-end'> {price} <span>
                             {
                                 price === ! "" ? "تومان" : ""
