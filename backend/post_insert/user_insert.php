0d67216d417dc81bb29e8bae80949043d2be9ffc
SAMURAI JACK <mr.amir1385mm@gmail.com>
2023-12-05 00:20:21 +0330

    some random shit

--- backend/post_insert/user_insert.php
+++ backend/post_insert/user_insert.php
@@ -6,6 +6,7 @@ echo "hi";
 include('../dbconfig/database.php');
 
 if (isset($_POST)) {
+
     $name = $_POST['name'];
     $password = $_POST['password'];
     $email = $_POST['email'];
@@ -15,9 +16,11 @@ if (isset($_POST)) {
     $card_number = $_POST['card_number'];
     $birth_date = $_POST['birth_date'];
 
-    $result = $con->prepare("INSERT INTO user SET `name`=$name , `password`=$password , `email`=$email , `phone number`=$phone_number , `home number`=$home_number , `national Code` =$national_Code , `card_number`=$card_number , `birth date`=$birth_date");
+    $result = $con->prepare("INSERT INTO user SET `name`=$name , `password`=$password , `email`=$email , `phone number`=$phone_number , `home number`=$home_number , `national Code` =$national_Code , `card number`=$card_number , `birth date`=$birth_date");
     $result->execute();
     $datas = $result->fetchAll(PDO::FETCH_ASSOC);
+
+    
 
 
 }
