4d8d0341ebb6a0658e8d262f1279c78aba1bb5d8
SAMURAI JACK <mr.amir1385mm@gmail.com>
2024-07-16 23:41:07 -0700

    API doc
    added API doc and its done now

--- backend/post_insert/product_insert.php
+++ backend/post_insert/product_insert.php
@@ -12,20 +12,22 @@ if (isset($_POST)) {
   $content = $_GET['content'];
   $price = $_GET['price'];
   $warranty = $_GET['warranty'];
-  $score = $_GET['score'];
-  $tags = $_GET['tags'];
+  $category = $_GET['category'];
+  $infos = $_GET['infos'];
   $quanity = $_GET['quanity'];
+  $off = $_GET['off'];
 
   try {
-    $result = $con->prepare("INSERT INTO product (`title`,`image`,`content`,`price`,`warranty`,`score`,`tags`,`quanity`) VALUES (?,?,?,?,?,?,?,?)");
+    $result = $con->prepare("INSERT INTO product (`title`,`image`,`content`,`price`,`warranty`,`infos`,`category`,`quanity`,`off`) VALUES (?,?,?,?,?,?,?,?,?)");
     $result->bindValue(1,$title);
     $result->bindValue(2,$image);
     $result->bindValue(3,$content);
     $result->bindValue(4,$price);
     $result->bindValue(5,$warranty);
-    $result->bindValue(6,$score);
-    $result->bindValue(7,$tags);
+    $result->bindValue(6,$infos);
+    $result->bindValue(7,$category);
     $result->bindValue(8,$quanity);
+    $result->bindValue(9,$off);
 
     $result->execute();
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
